30d21d217d6d085b02a4013b9c0bd4f5b3639e5c
Jose Granado <jose.granado2014@gmail.com>
2023-04-17 15:35:41 -0400

    First commit, montaje de ambiente de desarrollo y del tema a trabajar MyTravel

---
+++ storage/framework/views/e52529606b862540d4e52f7bb4a701fdf7ee76bc.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="bravo-text <?php echo e($class ?? ''); ?>">
+        <?php echo clean($content ?? ''); ?>
+
+    </div>
+</div><?php /**PATH C:\xampp\htdocs\acomodo\themes/Mytravel/Template/Views/frontend/blocks/text.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f8c20e4dc8922cba01c486119cb10593225db6a3.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="bravo-text <?php echo e($class ?? ''); ?>">
+        <?php echo clean($content ?? ''); ?>
+
+    </div>
+</div><?php /**PATH C:\Users\joseg\Documents\Github\AcomodoMyTravel\themes/Mytravel/Template/Views/frontend/blocks/text.blade.php ENDPATH**/ ?>
